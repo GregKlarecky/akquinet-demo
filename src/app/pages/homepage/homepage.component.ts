24f3eb49a1c4b938c562585b236c0a2b981c2b55
grzegorz.klarecki <grzegorz.klarecki@gmail.com>
2020-05-25 22:40:09 +0200

    app-post mobile basic

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ApiService } from "src/app/services/api.service";
+import { Post } from "src/app/interfaces/post.interface";
 
 @Component({
   selector: "app-homepage",
@@ -7,9 +8,12 @@ import { ApiService } from "src/app/services/api.service";
   styleUrls: ["./homepage.component.scss"]
 })
 export class HomepageComponent implements OnInit {
+  public postList: Post[];
   constructor(private api: ApiService) {}
 
   ngOnInit() {
-    this.api.getPosts().subscribe(console.log);
+    this.api.getPosts().subscribe(postList => {
+      this.postList = postList.posts;
+    });
   }
 }
