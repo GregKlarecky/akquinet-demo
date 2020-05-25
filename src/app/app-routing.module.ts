c1b886a0c3c7c8887d75b22ee0317f5328ce9e90
grzegorz.klarecki <grzegorz.klarecki@gmail.com>
2020-05-25 20:17:54 +0200

    bootstrap and variables set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomepageComponent } from "./pages/homepage/homepage.component";
+import { PostDetailsComponent } from "./pages/post-details/post-details.component";
 
-const routes: Routes = [{ path: "home", component: HomepageComponent }];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full"
+  },
+  { path: "home", component: HomepageComponent },
+  { path: "post/:id", component: PostDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
