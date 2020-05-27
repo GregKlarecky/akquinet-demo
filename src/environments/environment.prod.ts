30b4c3e65fbd0df18dc34197705e46c2b0d4e533
grzegorz.klarecki <grzegorz.klarecki@gmail.com>
2020-05-27 19:23:33 +0200

    readme

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: false
 };
+
+export const domain = "https://public-api.wordpress.com";
+export const site = "en.blog.wordpress.com";
+export const url = `${domain}/rest/v1.1/sites/${site}/`;
